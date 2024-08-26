0fd354e4c851fcd26d198e7fcd1447fe73d8c5c6
Yuvaraj-gowebez <yuva3102000@gmail.com>
2024-08-26 10:41:37 +0530

    Main || Correction

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_3.19.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\Flutter_App\gymapp"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\gymapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
